17f9b71856433d759fee20786c2fcc82181c96d6
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-07-23 08:11:34 +0530

    Time: 0 ms (100.00%) | Memory: 80 MB (89.88%) - LeetSync

---
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int len = arr.length;
+        int low = 0 , high = len-1;
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if(mid+1<len && mid-1 >= 0 && arr[mid+1] < arr[mid] && arr[mid-1] < arr[mid]){
+                return mid;
+            }
+            else if(mid+1 < len && arr[mid+1] < arr[mid]){
+                high = mid;
+            }
+            else if(mid-1 >= 0 && arr[mid-1] < arr[mid]){
+                low = mid;
+            }
+            else{
+                System.out.println("at low : " + low +" high : "+ high);
+                break;
+            }
+        }
+        return -1;
+    }
+}
