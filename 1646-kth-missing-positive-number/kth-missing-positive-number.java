6bc5d1c3d66188bbcedee9cb077f2192d3cbab26
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-07-29 23:07:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (57.95%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -3,19 +3,18 @@ class Solution {
         int l = 0 , r = arr.length-1;
         while(l <= r){
             int mid = l + (r-l)/2;
-            if(mid >= arr.length){
-                l = arr.length;
-                System.out.println("breaked here");
-                break;
-            }
-            else if(arr[mid]-(mid+1) >= k){
+            // if(mid >= arr.length){
+            //     l = arr.length;
+                
+            //     break;
+            // }
+            if(arr[mid]-(mid+1) >= k){
                 r = mid-1;
             }
             else{
                 l = mid+1;
             }
         }
-        System.out.println("l : " + l + " r : " + r);
         int res = -1;
         if(r == -1){
             res = k;
