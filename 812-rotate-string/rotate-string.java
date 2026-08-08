f39bec17aa9efcd9a250f8e6b8631883d733799c
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-08 10:42:30 +0530

    Time: 7 ms (1.15%) | Memory: 43.2 MB (74.19%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,8 +3,8 @@ class Solution {
         if(s.equals(goal)) return true;
 
         char[] arr = s.toCharArray();
-        for(int i = 0 ; i < s.length() ; i++){
-            rotate(arr);
+        for(int i = 1 ; i < s.length() ; i++){
+            rotate2(arr);
             String st = new String(arr);
             System.out.println(st);
             if(st.equals(goal)) return true;
@@ -13,10 +13,25 @@ class Solution {
         return false;
     }
 
+    void rotate2(char[] arr){
+        int len = arr.length;
+        for(int i = 0 ; i < len/2 ; i++){
+            char temp = arr[i];
+            arr[i] = arr[len-1-i];
+            arr[len-1-i] = temp;
+        }
+
+        for(int i = 0 ; i < (len-1)/2 ; i++){
+            char temp = arr[i];
+            arr[i] = arr[len-2-i];
+            arr[len-2-i] = temp;
+        }
+    }
+
     void rotate(char[] arr){
         char temp = arr[0];
         for(int i = 1 ; i < arr.length ; i++){
-            char temp1 = arr[i]; 
+            char temp1 = arr[i];
             arr[i] = temp;
             temp = temp1;
         }
