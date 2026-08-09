73e3d1201814a06ec642d1375bacc68ca5674901
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-09 21:57:57 +0530

    Time: 22 ms (33.08%) | Memory: 47 MB (21.91%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,34 +1,27 @@
 class Solution {
     public String longestPalindrome(String s) {
         int len = s.length();
-        int max = 0;
         String res = "";
-
-        for(int i = 0; i < len; i++){
+        for(int i = 0 ; i < len ; i++){
             //odd
-            int l = i , r = i;
-            while(l >= 0 && r < len && s.charAt(l) == s.charAt(r)){
-                l--;
-                r++;
+            int p1 = i , p2 = i;
+            while(p1 >= 0 && p2 < len && s.charAt(p1) == s.charAt(p2)){
+                if(res.length() < (p2-p1+1)){
+                    res = s.substring(p1 , p2+1);
+                }
+                p1--;
+                p2++;
             }
-
-            if(r-l-1 > max){
-                max = r-l-1;
-                res = s.substring(l+1 , r);
-            }
-            
 
             //even
-            l=i;
-            r = i+1;
-            while(l >= 0 && r < len && s.charAt(l) == s.charAt(r)){
-                l--;
-                r++;
-            }
-
-            if(r-l-1 > max){
-                max = r-l-1;
-                res = s.substring(l+1 , r);
+            p1 = i ;
+            p2 = i+1;
+            while(p1 >= 0 && p2 < len && s.charAt(p1) == s.charAt(p2)){
+                if(res.length() < (p2-p1+1)){
+                    res = s.substring(p1 , p2+1);
+                }
+                p1--;
+                p2++;
             }
         }
 
