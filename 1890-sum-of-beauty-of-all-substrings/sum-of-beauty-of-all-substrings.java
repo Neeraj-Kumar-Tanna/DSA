8906975a20dbe1936edd5b11bcaa98db2c266468
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-10 17:33:06 +0530

    Time: 852 ms (5.03%) | Memory: 46.9 MB (9.51%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,30 +1,24 @@
 class Solution {
     public int beautySum(String s) {
-        
         int len = s.length();
-        // int max = -1 , min = 505;
         int res = 0;
 
-        for(int i = 0; i < len ; i++){
-            HashMap<Character , Integer> mp = new HashMap<>();
+        for(int i = 0 ; i < len ;i++){
+            HashMap<Character , Integer> freq = new HashMap<>();
             for(int j = i ; j < len ; j++){
-                mp.put(s.charAt(j) , mp.getOrDefault(s.charAt(j) , 0) + 1);
-                int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
-                for(Map.Entry<Character , Integer> e : mp.entrySet()){
-                    max = max < e.getValue() ? e.getValue() : max;
-                    min = min > e.getValue() ? e.getValue() : min;
+                char temp = s.charAt(j);
+                freq.put(s.charAt(j) , freq.getOrDefault(s.charAt(j) , 0 )+1);
+                int max = Integer.MIN_VALUE , min = Integer.MAX_VALUE;
+
+                for(Map.Entry<Character , Integer> x : freq.entrySet()){
+                    max = max < x.getValue() ? x.getValue() : max;
+                    min = min > x.getValue() ? x.getValue() : min;
                 }
 
-                res += (max-min);
+                res += (max - min);
             }
-
-
-            // freq[s.charAt(i)]++;
-            // max = max < freq[s.charAt(i)] ? freq[s.charAt(i)] : max;
-            // min = min > freq[s.charAt(i)] ? freq[s.charAt(i)] : min;
         }
 
         return res;
-
     }
 }
