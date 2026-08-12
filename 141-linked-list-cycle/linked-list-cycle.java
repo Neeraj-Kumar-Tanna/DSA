a55107e8c326cc27c088844a734030b135d8b9dd
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-12 22:52:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (87.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -12,15 +12,29 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         boolean isLoop = false;
-        ListNode temp = head;
-        int count = 0 ;
-        while(temp != null){
-            count++;
-            if(count > 10000) return true;
+        ListNode temp1 = head;
+        ListNode temp2 = head;
 
-            temp = temp.next;
+        while(temp2 != null){
+            if(temp2.next == null) return false;
+            
+            temp1 = temp1.next;
+            temp2 = temp2.next.next;
+
+            if(temp1 == temp2) return true;
+
         }
 
         return false;
+
+
+        // int count = 0 ;
+        // while(temp != null){
+        //     count++;
+        //     if(count > 10000) return true;
+        //     temp = temp.next;
+        // }
+
+        // return false;
     }
 }
