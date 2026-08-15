7cac84c5a494d1ae6d7b70fd0c549edcd06437f1
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-15 14:40:26 +0530

    Time: 1 ms (9.93%) | Memory: 48.4 MB (11.71%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,29 +1,41 @@
 class Solution {
     public double myPow(double x, int m) {
-        double a = x;
-        double res = 1;
-        boolean isNeg = false;
+        // double a = x;
+        // double res = 1;
+        // boolean isNeg = false;
+        // if(m < 0){
+        //     isNeg = true;
+        // }
+
+        // long n = m;
+        // n = Math.abs(n);
+
+        // while(n > 0){
+        //     if((n&1) == 1){
+        //         res = res*a;
+        //     }
+        //     n >>= 1;
+        //     a = a*a;
+        //     System.out.println(a + "  : "+ res);
+        // }
+
+        // if(isNeg){
+        //     return 1/res;
+        // }
+
+        //----------------------------
+
+        
         if(m < 0){
-            isNeg = true;
-        }
+            double n = m;
+            n = Math.abs(n);
 
-        long n = m;
-        n = Math.abs(n);
-
-        while(n > 0){
-            if((n&1) == 1){
-                res = res*a;
-            }
-            n >>= 1;
-            a = a*a;
-            System.out.println(a + "  : "+ res);
-        }
-
-        if(isNeg){
+            double res = Math.pow(x , n);
             return 1/res;
         }
-
-        return res;
+        double n = m;
+        n = Math.abs(n);
+        return (double)Math.pow(x, n);
     }
 }
 
