58a0be6b5b7fcda9eff528ec9cc667a6193321cb
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-18 22:19:59 +0530

    Time: 17 ms (6.33%) | Memory: 54.6 MB (12.64%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -12,22 +12,49 @@
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         Set<ListNode> a = new HashSet<>();
+        Set<ListNode> b = new HashSet<>();
 
-        ListNode tempa = headA;
-        while(tempa != null){
-            a.add(tempa);
-            tempa = tempa.next;
-        }
-
-        tempa = headB;
-        while(tempa != null){
-            if(a.contains(tempa)){
-                return tempa;
+        while(headA != null || headB != null){
+            if(headA != null){
+                if(b.contains(headA)){
+                    return headA;
+                }
+                else{
+                    a.add(headA);
+                    headA = headA.next;
+                }
             }
-            tempa = tempa.next;
+            if(headB != null){
+                if(a.contains(headB)){
+                    return headB;
+                }
+                else{
+                    b.add(headB);
+                    headB = headB.next;
+                }
+            }
         }
 
         return null;
+
+        //------------APPROACH-1-----------------
+        // Set<ListNode> a = new HashSet<>();
+
+        // ListNode tempa = headA;
+        // while(tempa != null){
+        //     a.add(tempa);
+        //     tempa = tempa.next;
+        // }
+
+        // tempa = headB;
+        // while(tempa != null){
+        //     if(a.contains(tempa)){
+        //         return tempa;
+        //     }
+        //     tempa = tempa.next;
+        // }
+
+        // return null;
     }
 
 }
