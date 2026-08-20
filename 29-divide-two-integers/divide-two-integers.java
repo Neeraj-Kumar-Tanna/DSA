d2e861e003af9a33730a55d10f6ff54d4da951f6
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-20 23:51:55 +0530

    Time: 31 ms (2.76%) | Memory: 49.9 MB (14.53%) - LeetSync

---
+++ 29-divide-two-integers/divide-two-integers.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int divide(int n, int m) {
+        
+        if(n == m) return 1;
+
+        boolean pos = true;
+        if(n <= 0 && m >0) pos = false;
+        if(n >= 0 && m < 0) pos = false;
+
+        long dividend = Math.abs((long)n);
+        long divisor = Math.abs((long)m);
+
+        long ans = 0;
+
+        while(dividend >= divisor){
+            long count = 0;
+
+            while(dividend > (divisor<<(count+1))){
+                count++;
+                System.out.println((divisor<<(count+1)) + " cnt : " + count);
+            }
+
+            ans += (1<<count);
+            dividend = dividend - (divisor<<count);
+            System.out.println(dividend + " : ans " + ans + " vount : " + count);
+        }
+        System.out.println(1<<31);
+        if(ans > Integer.MAX_VALUE && pos == true){
+            return Integer.MAX_VALUE;
+        }
+        if(ans > Integer.MAX_VALUE && pos == false){
+            return Integer.MIN_VALUE;
+        }
+
+        return pos ? (int)ans : (int)-ans;
+    }
+}
