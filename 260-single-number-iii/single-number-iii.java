29e9fb50381ff7178a259d690aa7fa62e314f1f1
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-08-29 12:04:28 +0530

    Time: 1 ms (100.00%) | Memory: 48 MB (39.48%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -3,29 +3,51 @@ class Solution {
         int x = 0;
         boolean found = false;
         int res1 = 0 , res2 = 0;
-        int shift = 0;
-        while(!found){
-            for(int cur : nums){
-                x = x^((cur>>shift)&1);
-            }
-            if(x == 1){
-                for(int cur : nums){
-                    if(((cur>>shift)&1) == 1){
-                        res1 ^= cur;
-                    }
-                    else{
-                        res2^= cur;
-                    }
-                }
-                System.out.println(res1 + " " + res2 + " " + shift);
 
-                return new int[]{(res1) , (res2)};
-            }
-            System.out.println(shift + " x : " + x);
-            shift++;
-            x= 0;
+        for(int cur : nums){
+            x = x^cur;
         }
+
+        x = (x&(x-1))^x;
+
+        for(int cur : nums){
+            if((cur&x) == 0){
+                res1 ^= cur;
+            }
+            else{
+                res2 ^= cur;
+            }
+        }
+
+        return new int[]{res1 , res2};
+
+
+        //--------Approach#1--------------------------------
+
+        // int shift = 0;
+        // while(!found){
+        //     for(int cur : nums){
+        //         x = x^((cur)&(1<<shift));
+        //     }
+        //     x = x>>shift;
+        //     if(x == 1){
+        //         for(int cur : nums){
+        //             if(((cur)&(1<<shift)) == (1<<shift)){
+        //                 res1 ^= cur;
+        //             }
+        //             else{
+        //                 res2^= cur;
+        //             }
+        //         }
+        //         System.out.println(res1 + " " + res2 + " " + shift);
+
+        //         return new int[]{(res1) , (res2)};
+        //     }
+        //     System.out.println(shift + " x : " + x);
+        //     shift++;
+        //     x= 0;
+        // }
         
-        return new int[]{-1, -1};
+        // return new int[]{-1, -1};
     }
 }
