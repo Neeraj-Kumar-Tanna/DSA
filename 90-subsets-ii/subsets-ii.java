1761cac4dfdfd84e68a21e2c0e8628e53a64caf9
Tanna Neeraj Kumar <neeraj772006@gmail.com>
2026-09-04 15:34:07 +0530

    Time: 2 ms (99.73%) | Memory: 45.1 MB (42.23%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,25 +1,26 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
       List<List<Integer>> res = new ArrayList<>();
-      
+      Arrays.sort(nums);
       rec(0 , new ArrayList<>() , nums , res);
 
-      List<List<Integer>> uni = new ArrayList<>(new LinkedHashSet<>(res));
-
-      return uni;
-
+      return res;
     }
 
     private static void rec(int ind , List<Integer> data , int[] arr , List<List<Integer>> res){
+      res.add(new ArrayList<>(data));
       if(ind == arr.length){
-        List<Integer> temp = new ArrayList<>(data);
-        Collections.sort(temp);
-        res.add(temp);
+        // res.add(new ArrayList<>(data));
+        // System.out.println("len : "+ data);
         return;
       }
-      data.add(arr[ind]);
-      rec(ind+1 , data , arr , res);
-      data.removeLast();
-      rec(ind+1 , data , arr , res);
+
+      for(int i = ind ; i < arr.length ; i++){
+        if(i > ind && arr[i]==arr[i-1]) continue;
+        // System.out.println(data);
+        data.add(arr[i]);
+        rec(i+1 , data , arr, res);
+        data.removeLast();
+      }
     }
 }
